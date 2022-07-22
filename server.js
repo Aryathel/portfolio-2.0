81246668ea51ee2ef2f43ce1fc9bcc379959c3c1
Heroicos_HM <houghtonawe@gmail.com>
2022-07-22 08:14:11 -0700

    Server update for commonjs

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import { createServer } from "http";
-import next from "next";
+const { createServer } = require("http");
+const next = require("next");
 
 const isDevMode = process.env.NODE_ENV !== 'production';
 const port = process.env.PORT ? process.env.PORT : 3000;
