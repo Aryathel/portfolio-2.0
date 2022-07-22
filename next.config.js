223972b9b07ce18020707c599e980e3288cc10a6
Heroicos_HM <houghtonawe@gmail.com>
2022-07-22 08:18:25 -0700

    Updated for cpanel

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 module.exports = {
     reactStrictMode: true,
-    webpack5: true,
     webpack: (config) => {
         config.resolve.fallback = { fs: false };
 
         return config;
     },
     trailingSlash: true,
-    basePath: '',
 }
